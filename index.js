90bc4e0e24dcc04bffab4af156e84b89c5bc4cc5
patxol <pol@lemonbox.fr>
2022-01-02 22:51:25 +0100

    improvements

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
+  console.log("start");
+  let opt = [
+    { sa: 10, sp: 15, np: 11 },
+    { sa: 10, sp: 15, np: 16 },
+    { sa: 10, sp: 15, np: 26 },
+    { sa: 10, sp: 25, np: 6 },
+    { sa: 10, sp: 25, np: 11 },
+    { sa: 10, sp: 25, np: 16 },
+    { sa: 10, sp: 25, np: 26 },
+    { sa: 10, sp: 35, np: 6 },
+    { sa: 10, sp: 35, np: 11 },
+    { sa: 10, sp: 35, np: 16 },
+    { sa: 10, sp: 35, np: 26 },
+    { sa: 10, sp: 45, np: 6 },
+    { sa: 10, sp: 45, np: 11 },
+    { sa: 10, sp: 45, np: 16 },
+    { sa: 10, sp: 45, np: 26 },
+    { sa: 10, sp: 55, np: 6 },
+    { sa: 10, sp: 55, np: 11 },
+    { sa: 10, sp: 55, np: 16 },
+    { sa: 10, sp: 55, np: 26 },
+    { sa: 100, sp: 25, np: 6 },
+    { sa: 100, sp: 45, np: 6 },
+    { sa: 100, sp: 55, np: 6 },
+  ];
   let i = 8;
-  let j = 0;
-  for (let np = 1; np <= 5 * 6; np += 5) {
-    for (let sp = 5; sp <= 60; sp += 10) {
-      for (let sa = 1; sa < 10000; sa = sa * 10) {
-        const browser = await puppeteer.launch({
-          defaultViewport: { width: 100 * i, height: 100 * i },
-          // timeout: 0,
-        });
-        const page = await browser.newPage();
-        console.log(`building example-${sa}-${sp}-${np}.png`);
-        try {
-          await page.goto(
-            `http://127.0.0.1:5500/?seed=123456&stepA=${sa}&stepP=${sp}&noiseP=${np}`,
-            // "https://gateway.fxhash.xyz/ipfs/QmaN4onnUgFs2eDsriy2ns82jWMYm8ZnPHrbSkh5LmEfkP/?fxhash=op4QY7XrvdA7XzvXCw36L9kXhrZ5E4Fr4RPb79cHgC1NYmvx5XW",
-            {
-              waitUntil: "networkidle2",
-              timeout: 0,
-            }
-          );
-          await page.$("canvas#defaultCanvas0");
-          await page.screenshot({
-            path: `./output/example-${sa}-${sp}-${np}.png`,
-          });
-        } catch (error) {
-          console.error(error);
+  for (let j = 0; j < 50; j++) {
+    let seed = Math.floor(100000 * Math.random());
+    console.log(seed);
+
+    const browser = await puppeteer.launch({
+      defaultViewport: { width: 100 * i, height: 100 * i },
+      // timeout: 0,
+    });
+    const page = await browser.newPage();
+    console.log(`building example-${seed}.png`);
+    try {
+      await page.goto(
+        `http://127.0.0.1:5500/?seed=${seed}`,
+        // "https://gateway.fxhash.xyz/ipfs/QmaN4onnUgFs2eDsriy2ns82jWMYm8ZnPHrbSkh5LmEfkP/?fxhash=op4QY7XrvdA7XzvXCw36L9kXhrZ5E4Fr4RPb79cHgC1NYmvx5XW",
+        {
+          waitUntil: "networkidle2",
+          timeout: 0,
         }
-        await browser.close();
-        j++;
-      }
+      );
+      await page.$("canvas#defaultCanvas0");
+      await page.screenshot({
+        path: `./output/example-${seed}.png`,
+      });
+    } catch (error) {
+      console.error(error);
     }
+    await browser.close();
   }
 })();
